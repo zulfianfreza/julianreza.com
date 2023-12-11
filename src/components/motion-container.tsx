b440bb04176f8fb9606e53186bbb73720d705ad4
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-12-11 15:54:23 +0700

    update about page

--- src/components/motion-container.tsx
+++ src/components/motion-container.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { animate, initial, transition } from "@/lib/constants";
+import { transition } from "@/lib/constants";
 import { motion } from "framer-motion";
 
 export default function MotionContainer({
