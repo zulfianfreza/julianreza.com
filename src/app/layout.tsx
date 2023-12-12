a583ee1feaabc8ee7443437fd25c5863ea669e75
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-12-12 12:59:23 +0700

    update og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Julian Reza",
   metadataBase: new URL("https://julianreza.com/"),
-  description: "Hi, i'm julian. I'm a Fullstack Developer.",
+  description: "Hi, i'm Julian. I'm a Fullstack Developer.",
   icons: "/images/logo-black.png",
+  creator: "Zulfian Fachru  Reza",
+  publisher: "Zulfian Fachru Reza",
   openGraph: {
     title: "Julian Reza",
-    description: "Hi, i'm julian. I'm a fullstack developer.",
+    description: "Hi, i'm Julian. I'm a fullstack developer.",
     images: ["/images/og-image.png"],
   },
   twitter: { card: "summary_large_image", images: ["/images/og-image.png"] },
